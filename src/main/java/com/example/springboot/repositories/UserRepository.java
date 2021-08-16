7843740b2f52c09803bfad2027659a90d753f2e9
Vladdjuga <55984362+Vladdjuga@users.noreply.github.com>
2021-08-16 17:56:48 +0300

    sadasd

--- src/main/java/com/example/springboot/repositories/UserRepository.java
+++ src/main/java/com/example/springboot/repositories/UserRepository.java
@@ -1,7 +1,11 @@
 package com.example.springboot.repositories;
 
 import com.example.springboot.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+    User findByUsername(String username);
+
 }
